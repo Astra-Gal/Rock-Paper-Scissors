1b28a0145578d2872453037fda383bbbc950c93c
Astra-Gal <giulia.galastro@gmail.com>
2020-12-04 18:24:51 +0000

    second rock test passing

--- app/models/game.py
+++ app/models/game.py
@@ -4,3 +4,6 @@ from app.models.player import Player
 def play_rps(player1, player2):
     if player1.choice == "Rock" and player2.choice == "Scissors":
         return player1
+
+    if player1.choice == "Rock" and player2.choice == "Paper":
+        return player2
--- app/tests/game_test.py
+++ app/tests/game_test.py
@@ -9,6 +9,12 @@ class TestGame(unittest.TestCase):
         self.player2 = Player("Paper Balboa", "Paper")
         self.player3 = Player("Scissors Balboa", "Scissors")
 
+# rock tests
+
     def test_rock_beats_scissors(self):
         result = play_rps(self.player1, self.player3)
         self.assertEqual(self.player1, result)
+
+    def test_rock_loses_to_paper(self):
+        result = play_rps(self.player1, self.player2)
+        self.assertEqual(self.player2, result)

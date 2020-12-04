39e80c9d4ca47ee82c64e3ab4687dca235010cee
Astra-Gal <giulia.galastro@gmail.com>
2020-12-04 17:01:56 +0000

    second player test passed

--- app/tests/player_test.py
+++ app/tests/player_test.py
@@ -8,3 +8,6 @@ class TestPlayer(unittest.TestCase):
 
     def test_player_has_name(self):
         self.assertEqual("Rocky Balboa", self.player.name)
+
+    def test_player_has_choice(self):
+        self.assertEqual("Rock", self.player.choice)

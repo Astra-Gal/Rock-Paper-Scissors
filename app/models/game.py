dd6b257cbcae81383c8a46c4dc7878135d9c16ab
Astra-Gal <giulia.galastro@gmail.com>
2020-12-05 10:38:29 +0000

    refactored my game methods to include a winner

--- app/models/game.py
+++ app/models/game.py
@@ -3,19 +3,27 @@ from app.models.player import Player
 
 def play_rps(player1, player2):
     if player1.choice == "Rock" and player2.choice == "Scissors":
-        return player1
+        winner = player1
+        return winner
 
     if player1.choice == "Rock" and player2.choice == "Paper":
-        return player2
+        winner = player2
+        return winner
 
     if player1.choice == "Paper" and player2.choice == "Scissors":
-        return player2
+        winner = player2
+        return winner
 
     if player1.choice == "Paper" and player2.choice == "Rock":
-        return player1
+        winner = player1
+        return winner
 
     if player1.choice == "Scissors" and player2.choice == "Rock":
+        winner = player2
         return player2
 
     if player1.choice == "Scissors" and player2.choice == "Paper":
-        return player1
+        winner = player1
+        return winner
+
+    # can I get away with just one return statement at the end?

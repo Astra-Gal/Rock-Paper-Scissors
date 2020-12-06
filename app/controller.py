5766e798317659f2978aacbeb247f51cba05f487
Astra-Gal <giulia.galastro@gmail.com>
2020-12-06 19:54:02 +0000

    got it to display result and redirect to home page to play again

--- app/controller.py
+++ app/controller.py
@@ -1,4 +1,4 @@
-from flask import render_template, request, redirect
+from flask import render_template, request, redirect, url_for
 from app import app
 from app.models.game import *
 from app.models.player import *
@@ -7,10 +7,8 @@ from app.models.player import *
 def index():
     return render_template('index.html', title='Home')
 
-# @app.route('/play/<player1>/<player2>')
-# def play(player1, player2):
-#     return f"The winner is {play_rps(player1, player2)}"
 
+# this returns an f string to the browser with the name and choice of the victor
 @app.route('/play-game', methods=['POST'])
 def play_game():
     name1 = request.form['player1-name']
@@ -20,10 +18,9 @@ def play_game():
     player1 = Player(name1, choice1)
     player2 = Player(name2, choice2)
     game = Game(player1, player1)
-    # return f"{play_rps(player1, player2)} wins!"
-    return f"{game.check_winner(player1, player2).name} wins!"
-
-
-       
-
-# next step: include their weapon of choice in the string!
+    result = play_rps(player1, player2)
+    if result != None:
+        return render_template('display_result.html', result=result)
+        # return f"{result.name} wins with {result.choice.lower()}!"
+    else:
+        return render_template('draw.html')
--- app/models/game.py
+++ app/models/game.py
@@ -9,29 +9,29 @@ class Game:
         return player1
 
 
-# def play_rps(player1, player2):
-#     if player1.choice == "Rock" and player2.choice == "Scissors":
-#         winner = player1
-#         return winner
+def play_rps(player1, player2):
+    if player1.choice == "Rock" and player2.choice == "Scissors":
+        winner = player1
+        return winner
 
-#     if player1.choice == "Rock" and player2.choice == "Paper":
-#         winner = player2
-#         return winner
+    if player1.choice == "Rock" and player2.choice == "Paper":
+        winner = player2
+        return winner
 
-#     if player1.choice == "Paper" and player2.choice == "Scissors":
-#         winner = player2
-#         return winner
+    if player1.choice == "Paper" and player2.choice == "Scissors":
+        winner = player2
+        return winner
 
-#     if player1.choice == "Paper" and player2.choice == "Rock":
-#         winner = player1
-#         return winner
+    if player1.choice == "Paper" and player2.choice == "Rock":
+        winner = player1
+        return winner
 
-#     if player1.choice == "Scissors" and player2.choice == "Rock":
-#         winner = player2
-#         return player2
+    if player1.choice == "Scissors" and player2.choice == "Rock":
+        winner = player2
+        return player2
 
-#     if player1.choice == "Scissors" and player2.choice == "Paper":
-#         winner = player1
-#         return winner
+    if player1.choice == "Scissors" and player2.choice == "Paper":
+        winner = player1
+        return winner
 
     # can I get away with just one return statement at the end?
